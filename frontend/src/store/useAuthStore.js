e05051773549c051caa6ee103ad257df61c789c1
Mohak-787 <mohakdevkota62@gmail.com>
2026-02-18 16:04:44 +0545

    feat: signup page ui and functionalities

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,38 @@
 import { create } from 'zustand'
+import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast"
 
 export const useAuthStore = create((set) => ({
-  authUser: { name: "", _id: 3, age: 0 },
-  isLoggedIn: false,
-  isLoading: false,
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
 
-  login: () => {
-    console.log("We just logged in"),
-      set({ isLoggedIn: true, isLoading: true })
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check")
+      set({ authUser: res.data })
+    } catch (error) {
+      console.error("Error checking auth in client side: ", error)
+      set({ authUser: null })
+    } finally {
+      set({ isCheckingAuth: false })
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true })
+    try {
+      const res = await axiosInstance.post("/auth/signup", data)
+      set({ authUser: res.data })
+
+      toast.success("Account created successfully!")
+    } catch (error) {
+      console.error("Error signing user in client side: ", error)
+      set({ authUser: null })
+
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isSigningUp: false })
+    }
   }
 }))
